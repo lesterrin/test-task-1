0d789709866b2e3b3b53ea1619ee765fdfd11a6f
v.shorinadm <voldemar-96@mail.ru>
2022-06-29 12:48:38 +1000

    Реализована переадрессация и авторизация

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Navigate, useNavigate} from 'react-router-dom';
+import auth from "../../../services/auth";
 
 const Login = ({isLoggedIn, setIsLoggedIn}) => {
 
-        const navigate = useNavigate();
+    const navigate = useNavigate();
 
-        if(isLoggedIn){
-           navigate('/profile');
+    const authorisation = () => {
+        let username = document.querySelectorAll('input[name=username]')[0].value;
+        let password = document.querySelectorAll('input[name=pass]')[0].value;
+        let isAuth = auth(username,password);
+        if (isAuth){
+            setIsLoggedIn(true);
         }
-
         else{
-            return(
-                <button
-                    className="btn btn-primary" onClick={() => setIsLoggedIn(true)}>
-                    Login</button>
-                /*<div>
-                    <h1>login page</h1>
-                        <div className="form">
-                            <label htmlFor="username"><b>User name</b></label>
-                            <input type="text" name="username"/>
-                            <label htmlFor="pass"><b>Password</b></label>
-                            <input type="password" name="pass"/>
-                            <button>Log In</button>
-                        </div>
-                    </div>*/
-        );
+            console.log('Ошибка');
         }
+    }
 
+    //необходимо переписать так, чтобы страница не рендерилась, если пользователь залогинен
+    useEffect(() => {
+        if(isLoggedIn){
+            navigate('/profile');
+        }
+    });
+
+
+    return(
+        <div>
+            <h1>login page</h1>
+            <div className="form">
+                <label htmlFor="username"><b>User name</b></label>
+                <input type="text" name="username"/>
+                <br/>
+                <br/>
+                <label htmlFor="pass"><b>Password</b></label>
+                <input type="password" name="pass"/>
+                <br/>
+                <br/>
+                <button onClick={() => authorisation()}>Login</button>
+            </div>
+        </div>
+    );
 }
-
 export default Login;
---
+++ src/services/auth.js
@@ -0,0 +1,13 @@
+const auth = (username, password) => {/*
+    if(username == 'admin' && password == '12345'){
+        return(true);
+    }
+    else{
+        return(false);
+    }  */
+
+    return (username == 'admin' && password == '12345');
+    //return isAuth;
+}
+
+export default auth;
